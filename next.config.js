64250946c2b585b4248898f571fdb06ed4b2ddee
reinatch <reinatchnatch@gmail.com>
2023-11-04 02:03:06 +0000

    git ignore

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
