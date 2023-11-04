5b5379970e24da8dbf8eaede464610557a634a1c
reinatch <reinatchnatch@gmail.com>
2023-11-04 03:30:32 +0000

    g

--- app/components/test.tsx
+++ app/components/test.tsx
@@ -8,7 +8,7 @@ import { Mesh } from "three";
 const config = require("../../next.config");
 
 function MeshComponent() {
-  const fileUrl = `${config.basePath}"/shiba/scene.glb"`;
+  const fileUrl = `${config.basePath}/shiba/scene.glb`;
   const mesh = useRef<Mesh>(null!);
   const gltf = useLoader(GLTFLoader, fileUrl);
   useFrame(() => {
