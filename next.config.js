9e94d5e67d08b63510c550a5f84b6862a671ea18
reinatch <reinatchnatch@gmail.com>
2023-11-04 02:07:14 +0000

    h

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // Optional: Change the output directory `out` -> `dist`
   distDir: "dist",
 };
-
 module.exports = nextConfig;
